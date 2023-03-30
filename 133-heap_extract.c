2f98ab51fb723372fdbca890baa2ca1b43c266b7
Toluthedev <enochjesse884@gmail.com>
2023-03-30 07:53:21 +0100

    133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -8,5 +8,43 @@
  */
 int heap_extract(heap_t **root)
 {
-	return (root);
+    int value;
+    heap_t *last, *temp;
+
+    if (!root || !(*root))
+        return 0;
+
+    last = (*root);
+    while (last->right)
+        last = last->right;
+
+    if (last == (*root))
+    {
+        value = last->n;
+        free(*root);
+        *root = NULL;
+        return value;
+    }
+
+    temp = (*root);
+    while (temp->right != last)
+        temp = temp->right;
+
+    value = (*root)->n;
+    (*root)->n = last->n;
+    temp->right = NULL;
+    free(last);
+
+    for (temp = (*root); temp; temp = temp->left)
+    {
+        if (temp->right && temp->right->n > temp->n)
+        {
+            swap_nodes(temp, temp->right);
+            temp = temp->right;
+        }
+        if (temp->left && temp->left->n > temp->n)
+            swap_nodes(temp, temp->left);
+    }
+
+    return value;
 }
